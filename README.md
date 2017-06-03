3374f4cbe04282593d6a6f2f0f90e87a8fecb8ea
Josh Manders <josh@joshmanders.com>
2017-06-02 22:40:14 -0500

    v2.2.10

---
+++ README.md
@@ -0,0 +1,53 @@
+# Semantic UI, converted to Sass
+
+`semantic-ui-sass` is a Sass-powered version of [Semantic UI](http://www.semantic-ui.com) and ready to drop into any project.
+
+## NOTE
+
+The package only has the default theme.
+
+## Installation and Usage
+
+```shell
+yarn add --dev semantic-ui-sass
+```
+
+### CSS
+
+Import Semantic in an SCSS file (for example, `application.scss`) to get all of Semantic's styles
+
+```css
+@import "semantic-ui";
+```
+
+### Custom Font
+
+```css
+$font-url: 'http://fonts.useso.com/css?family=Lato:400,700,400italic,700italic&subset=latin';
+@import 'semantic-ui';
+```
+
+### Skip font loading
+
+```css
+$import-google-fonts: false;
+@import 'semantic-ui';
+```
+
+### Custom font family
+
+```css
+$font-family: 'custom-font-family';
+@import 'semantic-ui';
+```
+
+### Customizable Variables
+
+```css
+$import-google-fonts: true !default;
+$font-url: 'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin,latin-ext' !default;
+$font-name: 'Lato' !default;
+$font-family: $font-name, 'Helvetica Neue', Arial, Helvetica, sans-serif !default;
+$icons-font-path: '../../icons' !default;
+$flags-image-path: '../../images' !default;
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,54 @@
+import gulp from 'gulp';
+import del from 'del';
+import run from 'run-sequence';
+import replace from 'gulp-replace';
+import { append } from 'gulp-insert';
+import { log } from 'gulp-util';
+import { clone } from 'gulp-git';
+
+const SRC_REPO = 'https://github.com/doabit/semantic-ui-sass';
+const TEMP_DIR = '.temp';
+
+gulp.task('clean', () => {
+  return del(['./.temp', './semantic-ui.scss', './semantic-ui', './icons', './images']);
+});
+
+gulp.task('fetch', (done) => {
+  clone(SRC_REPO, { args: TEMP_DIR, quiet: true }, done);
+});
+
+gulp.task('move-scss', () => {
+  return gulp.src(`${TEMP_DIR}/app/assets/stylesheets/**/*.scss`)
+    .pipe(replace(/semantic-ui\/iconsyarn/g, '#{\$icons-font-path}/icons'))
+    .pipe(replace(/semantic-ui\/flags\.png/g, '#{\$flags-image-path}/flags.png'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('move-images', () => {
+  return gulp.src(`${TEMP_DIR}/app/assets/images/semantic-ui/**/*`)
+    .pipe(gulp.dest('./images/'));
+});
+
+gulp.task('move-icons', () => {
+  return gulp.src(`${TEMP_DIR}/app/assets/fonts/semantic-ui/**/*`)
+    .pipe(gulp.dest('./icons/'));
+});
+
+gulp.task('append-to-variables', () => {
+  return gulp.src('./semantic-ui/globals/_variables.scss')
+    .pipe(append(`$icons-font-path: '../../icons' !default;\n`))
+    .pipe(append(`$flags-image-path: '../../images' !default;\n`))
+    .pipe(gulp.dest('./semantic-ui/globals'));
+});
+
+gulp.task('build', (done) => {
+  run('clean', 'fetch', ['move-scss', 'move-images', 'move-icons'], 'append-to-variables', done);
+});
+
+gulp.task('test', () => {
+  return log('testing...');
+});
+
+gulp.task('release', () => {
+  return log('releasing...');
+});
